c2c43aab3837df58c2c0f1c7f98320024bb2f7b5
natashankaranja <natashankaranja@gmail.com>
2025-05-20 15:33:39 +0300

    added total revenue

--- customers-database.sql
+++ customers-database.sql
@@ -6,3 +6,6 @@ CREATE TABLE products (product_id SERIAL PRIMARY KEY, name VARCHAR(100) price FL
 
 --ORDERS TABLE
 CREATE TABLE orders (order_id SERIAL PRIMARY KEY, customer_id INT, product_id INT,quantity INT, order_date DATE, FOREIGN KEY (customer_id) REFERENCES customers(customer_id), FOREIGN KEY (product_id) REFERENCES products(product_id) );
+
+--TOTAL REVENUE GENERATED FROM ORDERS
+SELECT SUM(p.price * o.quantity) AS total_revenue FROM orders o JOIN products p ON o.product_id = p.product_id;
