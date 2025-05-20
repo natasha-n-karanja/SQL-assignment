9f215c5606ff754d9f3360a7f5155fb3a28fec5c
natashankaranja <natashankaranja@gmail.com>
2025-05-20 15:27:35 +0300

    created orders table

--- customers-database.sql
+++ customers-database.sql
@@ -3,3 +3,6 @@ CREATE TABLE customers( customer_id SERIAL PRIMARY KEY, name VARCHAR(100), email
 
 --PRODUCTS TABLE
 CREATE TABLE products (product_id SERIAL PRIMARY KEY, name VARCHAR(100) price FLOAT);
+
+--ORDERS TABLE
+CREATE TABLE orders (order_id SERIAL PRIMARY KEY, customer_id INT, product_id INT,quantity INT, order_date DATE, FOREIGN KEY (customer_id) REFERENCES customers(customer_id), FOREIGN KEY (product_id) REFERENCES products(product_id) );
